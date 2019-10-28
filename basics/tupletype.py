1eabefdc16e64d237a5e27caaf0556c912efd058
Paula <paula.rp93@gmail.com>
2019-10-28 19:16:28 +0100

    Tuple type

---
+++ basics/tupletype.py
@@ -0,0 +1,14 @@
+tpl = (40,50,60,70)
+
+print(tpl)
+print(tpl[2])
+print(tpl*3)
+print(tpl.count(40))
+print(tpl.index(40))
+
+#List to tuple
+
+lst=[1,2,3,"abc"]
+print(type(list))
+tpl1 = tuple(lst)
+print(type(tpl1))
