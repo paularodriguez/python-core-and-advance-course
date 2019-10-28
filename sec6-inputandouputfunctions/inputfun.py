80d5285a1032268994bc50e59fff9f1ee6161ccd
Paula <paula.rp93@gmail.com>
2019-10-28 19:25:19 +0100

    Section 6: Input and output functions

---
+++ sec6-inputandouputfunctions/inputfun.py
@@ -0,0 +1,12 @@
+#s=input()
+#print(s)
+
+#name=input("Enter your name:")
+#print(name)
+
+#i = int(input("Enter your age:"))
+#print(i)
+#print(type(i))
+
+lst = [float(x) for x in input("Enter three numbers separated by comma:").split(",")]
+print(lst)
---
+++ sec6-inputandouputfunctions/printfun.py
@@ -0,0 +1,15 @@
+print("Hello")
+print("My name \n is Paula")
+a, b = 10, 20
+print(a, b) #space by default
+
+print(a, b,sep=",")
+
+name = "John"
+marks=94.5
+
+print(name, marks)
+
+print('Name is %s, marks are %0.2f'%(name, marks))
+print('Name is {}, marks are {}'.format(name, marks))
+print('Name is {0}, marks are {1}'.format(name, marks))
