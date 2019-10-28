7ff9d85d5751f59c47a5a6c07e99d939ae5c44e1
Paula <paula.rp93@gmail.com>
2019-10-28 19:26:43 +0100

    Section 7: Basic programs with input and ouptut funcions

---
+++ sec7-moreprograms/areaofacircle.py
@@ -0,0 +1,7 @@
+import math
+
+radio = float(input("Enter the radius: "))
+pi = math.pi
+area = pi * radio**2
+
+print("The area is: ", area)
---
+++ sec7-moreprograms/studentdetails.py
@@ -0,0 +1,5 @@
+studentId = int(input("Enter student id"))
+name = input("Enter student name")
+marks = float(input("Enter marks scored"))
+
+print("Student ID: {}, Name: {}, Marks: {}".format(studentId, name, marks))
