b7ae50452c902f056d551674ee255d5e2f45657e
Paula <paula.rp93@gmail.com>
2019-11-12 19:20:04 +0100

    Section 20: Regular Expressions - Sequence Characters

---
+++ sec20-regularexpressions/intro.py
@@ -0,0 +1,15 @@
+# Sequence Characters
+
+# \d -> ANY digit 0 to 9
+# \D -> ANY non-digit character
+
+# \s -> White space in the given string
+# \S -> No white space
+
+# \w -> Any alphanumeric value
+# \W -> No alphanumeric character
+
+# \b -> A space around words
+
+# \A -> Matches only at the start of the string
+# \Z -> Matches only at the end of the string
