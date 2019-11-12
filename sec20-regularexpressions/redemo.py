e78719d5ccc1f4859daf7c960bf3a2e46499bb44
Paula <paula.rp93@gmail.com>
2019-11-12 20:32:32 +0100

    Section 20: Regular Expressions - Findall and match

--- sec20-regularexpressions/redemo.py
+++ sec20-regularexpressions/redemo.py
@@ -1,6 +1,8 @@
 import re
 
-str = "Take up one idea.One idea at a time"
+str = "Take up one idea.one idea at a time"
+
+# FIND
 
 # Find 'o' followed by any two characters
 result = re.search(r'o\w\w', str)
@@ -11,3 +13,24 @@ print(result.group())
 # Find 'o' followed by any one character
 result = re.search(r'o\w', str)
 print(result.group())
+
+# No results -> None
+
+# SEARCH
+
+# Search 'o' followed by any two characters
+# Returns one list with all found substrings
+result = re.findall(r'o\w\w', str)
+
+print(result)
+
+# No results -> Empty list []
+
+# MATCH
+# Match method only searches at the beginning of the given string
+result = re.match(r'o\w\w', str)
+print(result)
+
+result = re.match(r'T\w\w', str)
+print(result.group())
+
