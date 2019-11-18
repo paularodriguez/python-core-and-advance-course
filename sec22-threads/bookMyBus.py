f40dd1615f87ef588c1ef0f73d27658103eee89a
Paula <paula.rp93@gmail.com>
2019-11-18 19:15:06 +0100

    Section 22: Threads - TicketBooking usecase

---
+++ sec22-threads/bookMyBus.py
@@ -0,0 +1,19 @@
+from threading import *
+
+class BookMyBus:
+
+    def buy(self):
+        print(current_thread())
+        print("Confirming a seat")
+        print("Processing the payment")
+        print("Printing the ticket")
+
+obj = BookMyBus()
+
+t1 = Thread(target=obj.buy)
+t2 = Thread(target=obj.buy)
+t3 = Thread(target=obj.buy)
+
+t1.start()
+t2.start()
+t3.start()
