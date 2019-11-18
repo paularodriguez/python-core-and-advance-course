41070a73cd2b7a388476fe1a4c8fb56253e65822
Paula <paula.rp93@gmail.com>
2019-11-18 20:03:59 +0100

    Section 22: Threads - Synchronization using lock

--- sec22-threads/bookMyBus.py
+++ sec22-threads/bookMyBus.py
@@ -2,17 +2,30 @@ from threading import *
 
 class BookMyBus:
 
-    def buy(self):
-        print(current_thread())
-        print("Confirming a seat")
-        print("Processing the payment")
-        print("Printing the ticket")
+    def __init__(self, availableSeats):
+        self.availableSeats = availableSeats
+        self.l = Lock()
 
-obj = BookMyBus()
+    def buy(self, seatsRequested):
+        # print(current_thread())
+        self.l.acquire()
 
-t1 = Thread(target=obj.buy)
-t2 = Thread(target=obj.buy)
-t3 = Thread(target=obj.buy)
+        print("Total seats available: ", self.availableSeats)
+        if self.availableSeats >= seatsRequested:
+            print("Confirming a seat")
+            print("Processing the payment")
+            print("Printing the ticket")
+            self.availableSeats -= seatsRequested
+        else:
+            print("Sorry.  No seats available")
+        self.l.release()
+
+
+obj = BookMyBus(10)
+
+t1 = Thread(target=obj.buy, args=(3,)) # We add one comma because it expects one list
+t2 = Thread(target=obj.buy, args=(4,))
+t3 = Thread(target=obj.buy, args=(5,))
 
 t1.start()
 t2.start()
